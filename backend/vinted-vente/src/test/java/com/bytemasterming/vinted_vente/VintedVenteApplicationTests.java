b4cc8c5652414393bb7596182137998f36ea69dc
ManichUser <151421854+ManichUser@users.noreply.github.com>
2025-05-20 21:39:48 +0100

    Initial commit du projetBd

---
+++ backend/vinted-vente/src/test/java/com/bytemasterming/vinted_vente/VintedVenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytemasterming.vinted_vente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintedVenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
