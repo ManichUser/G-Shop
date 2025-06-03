c2079b643a33c6fb0730bb6befeaeac87fbcf4b2
Coded23 <darwinjovian7@gmail.com>
2025-06-04 00:29:32 +0100

    service d'authentification avec spring security

---
+++ backend/authentification_test/src/test/java/com/example/authentification_test/controller/AuthControllerIT.java
@@ -0,0 +1,57 @@
+package com.example.authentification_test.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) @AutoConfigureMockMvc
+class AuthControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldRegisterUserSuccessfully() throws Exception {
+        String json = """
+    {
+        \"firstName\": \"Jane\",
+        \"lastName\": \"Doe\",
+        \"email\": \"jane@example.com\",
+        \"password\": \"pass1234\",
+        \"phoneNumber\": \"690123456\",
+        \"adress\": \"Bafoussam\"
+    }
+    """;
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").exists())
+                .andExpect(jsonPath("$.currentRole").value("USER"));
+    }
+
+    @Test
+    void shouldFailLoginWithInvalidUser() throws Exception {
+        String login = """
+    {
+        \"email\": \"wrong@example.com\",
+        \"password\": \"wrongpass\"
+    }
+    """;
+
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(login))
+                .andExpect(status().isBadRequest());
+    }
+
+}
+
